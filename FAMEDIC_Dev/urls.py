7f9c981eac894e0259c608208643c2d4d2a4eb32
RicardoQuintana <ricardoq@tics-sas.com>
2020-12-10 16:53:10 -0500

    Password

--- FAMEDIC_Dev/urls.py
+++ FAMEDIC_Dev/urls.py
@@ -127,6 +127,7 @@ urlpatterns = [
     # Vista al haber realizado radicación exitosa
     path('main/done/', portal_views.radicacion_finish, name='radicado_finished'),
 
+
     # Vista cambio de contraseña
     path('passwordchangesave/', portal_views.passwordchangesave, name='passwordchangesave'),
 ]
